d91ee461b851d7708937dd452d23c92ef5ca835f
Louise Ribeiro <101759423+rblouise@users.noreply.github.com>
2022-10-12 01:25:10 -0300

    Update LAB3.cs

--- código/LAB3.cs
+++ código/LAB3.cs
@@ -1,5 +1,73 @@
 // ordenação binária com pos
 
+namespace OrdenacaoBinaria
+{
+    internal class Program
+    {
+        static public void PrincipalOrdenação(int[] numeros, int esquerda, int meio, int direita)
+        {
+            int[] temp = new int[25];
+            int i, eol, num, pos;
+            eol = (meio - 1);
+            pos = esquerda;
+            num = (direita - esquerda + 1);
+
+            while ((esquerda <= eol) && (meio <= direita))
+            {
+                if (numeros[esquerda] <= numeros[meio])
+                    temp[pos++] = numeros[esquerda++];
+                else
+                    temp[pos++] = numeros[meio++];
+            }
+            while (esquerda <= eol)
+                temp[pos++] = numeros[esquerda++];
+            while (meio <= direita)
+                temp[pos++] = numeros[meio++];
+            for (i = 0; i < num; i++)
+            {
+                numeros[direita] = temp[direita];
+                direita--;
+            }
+        }
+
+        static public void MergeSort(int[] numeros, int esquerda, int direita)
+        {
+            int meio;
+            if (direita > esquerda)
+            {
+                meio = (direita + esquerda) / 2;
+                MergeSort(numeros, esquerda, meio);
+                MergeSort(numeros, (meio + 1), direita);
+                PrincipalOrdenação(numeros, esquerda, (meio + 1), direita);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Digite quantidade de elementos do vetor que deseja ordenar:");
+            int max = Convert.ToInt32(Console.ReadLine());
+            int[] numeros = new int[max];
+            for (int i = 0; i < max; i++)
+            {
+                Console.Write("\nEnter [" + (i + 1).ToString() + "] element: ");
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("Vetor Inicial:");
+            Console.Write("\n");
+            for (int k = 0; k < max; k++)
+            {
+                Console.Write(numeros[k] + " ");
+                Console.Write("\n");
+            }
+            Console.WriteLine("Vetor Ordenado:");
+            MergeSort(numeros, 0, max - 1);
+            for (int i = 0; i < max; i++)
+                Console.WriteLine(numeros[i]);
+
+        }
+    }
+}
+
 // ordenação binária sem pos
 
 // busca binária sem pos
