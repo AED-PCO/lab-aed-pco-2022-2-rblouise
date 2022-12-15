0c85c9c5322476ba73107d1801a26c57f9513cb1
Louise Ribeiro <101759423+rblouise@users.noreply.github.com>
2022-12-15 17:25:19 -0300

    Update LAB11.cs
    corrigindo codigo

--- código/LAB11.cs
+++ código/LAB11.cs
@@ -1,172 +1,84 @@
 //LISTA DUPLAMENTE ENCADEADA DINAMICA
 
-using PosicaoLista;
-using ListaClasse;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections;
 
-namespace ListaEncadeada
+namespace ListaDinamica
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            string Opcao;
-            int Valor;
+            ListaDupla listas = new ListaDupla();
 
-            Posicao Posicao_Aed = new Posicao();
+            string continuar = "";
+            
 
-            do
+            do{
+            int valor = 0;
+            int pos = 0;
+            string especifica = "";
+            int opcao = 0;
+
+            Console.WriteLine("====== LISTA DUPLA DINAMICA ======");
+            Console.WriteLine(" ");
+            Console.WriteLine("Escolha uma opção: ");
+            Console.WriteLine("1- Inserir.");
+            Console.WriteLine("2- Remover.");
+            Console.WriteLine("3- Sair");
+            opcao = int.Parse(Console.ReadLine());
+
+            if(opcao == 1)
             {
-                Console.WriteLine("");
-                Console.WriteLine("Bem vindo ao programa de listas.");
-                Console.WriteLine("Lembre-se você pode ir embora a qualquer momento! Nossas chamadas são aleatorias ou por ordem de sorte");
-                Console.WriteLine("Menu para utilizar o programa:");
-                Console.WriteLine("1 - Inserir no inicio da lista");
-                Console.WriteLine("2 - Inserir no final da lista");
-                Console.WriteLine("3 - Remover no inicio da lista");
-                Console.WriteLine("4 - Remover no final da lista");
-                Console.WriteLine("5 - Imprimir Elementos na Lista");
-                Console.WriteLine("");
+                Console.WriteLine("Informe um valor para inserir: ");
+                valor = int.Parse(Console.ReadLine());
 
-                Opcao = Console.ReadLine();
-
-                switch (Opcao)
+                Console.WriteLine("Dseja inserir em uma posição especifica? ");
+                especifica = Console.ReadLine();
+                especifica = especifica.ToUpper();
+                
+                if(especifica == "S")
+                {   
+                    Console.WriteLine("Informe a posição: ");
+                    pos = int.Parse(Console.ReadLine());
+                    listas.inserir(valor,pos);
+                }else if(especifica == "N")
                 {
-                    case "1":
-                        Console.Write("Qual o elemento para inserir na lista");
-                        Valor = int.Parse(Console.ReadLine());
-                        Lista.InserirInicio(Valor);
-                        break;
-                    case "2":
-                        Console.Write("Qual o elemento para inserir na lista");
-                        Valor = int.Parse(Console.ReadLine());
-                        Lista.InserirFim(Valor);
-                        break;
-                    case "3":
-                        Console.WriteLine("Qual o elemento para remoção da lista:");
-                        Valor = int.Parse(Console.ReadLine());
-                        Lista.RemoverInicio(Valor);
-                        Console.ReadKey();
-                        break;
-                    case "4":
-                        Console.WriteLine("Qual o elemento para remoção da lista:");
-                        Valor = int.Parse(Console.ReadLine());
-                        Lista.RemoverFim(Valor);
-                        Console.ReadKey();
-                        break;
-                    case "5":
-                        Lista.Imprimir();
-                        Console.ReadKey();
-                        break;
-
+                    listas.inserirFim(valor);
+                }else
+                {
+                    listas.inserirFim(valor);
+                    Console.WriteLine("Posiçao invalida");
                 }
-                Console.Clear();
-            } while (Opcao != "9");
-        }
-    }
-}
-using System;
-using ListaClasse;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+                 listas.imprimir();    
 
-namespace PosicaoLista
-{
-    internal class Posicao
-    {
-        public int Elemento;
-        public Posicao Apontador;
+            }
+            
+            Console.WriteLine(" ");
 
-        public Posicao()
-        {
-            this.Elemento = 0;
-        }
-
-        public Posicao(int Elemento)
-        {
-            this.Elemento = Elemento;
-        }
-    }
-}
-using PosicaoLista;
-using ListaClasse;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections;
-
-namespace ClasseFila
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            string Opcao;
-            int Valor;
-
-            Posicao Posicao_Aed = new Posicao();
-
-            do
+            if(opcao == 2)
             {
-                Console.WriteLine("");
-                Console.WriteLine("Bem vindo ao programa de listas.");
-                Console.WriteLine("Lembre-se você pode ir embora a qualquer momento! Nossas chamadas são aleatorias ou por ordem de sorte");
-                Console.WriteLine("Menu para utilizar o programa:");
-                Console.WriteLine("1 - Inserir no inicio da lista");
-                Console.WriteLine("2 - Inserir no final da lista");
-                Console.WriteLine("3 - Remover no inicio da lista");
-                Console.WriteLine("4 - Remover no final da lista");
-                Console.WriteLine("5 - Imprimir Elementos na Lista");
-                Console.WriteLine("");
+                Console.WriteLine("Qual posição deseja remover? ");
+                pos = int.Parse(Console.ReadLine());
+                Console.WriteLine("Valor excluido "+ listas.remover(pos));
+            }
 
-                Opcao = Console.ReadLine();
+            if(opcao == 3){
+                break;
+            }
 
-                switch (Opcao)
-                {
-                    case "1":
-                        Console.Write("Qual o elemento para inserir na lista");
-                        Valor = int.Parse(Console.ReadLine());
-                        Lista.InserirInicio(Valor);
-                        break;
-                    case "2":
-                        Console.Write("Qual o elemento para inserir na lista");
-                        Valor = int.Parse(Console.ReadLine());
-                        Lista.InserirFim(Valor);
-                        break;
-                    case "3":
-                        Console.WriteLine("Qual o elemento para remoção da lista:");
-                        Valor = int.Parse(Console.ReadLine());
-                        Lista.RemoverInicio(Valor);
-                        Console.ReadKey();
-                        break;
-                    case "4":
-                        Console.WriteLine("Qual o elemento para remoção da lista:");
-                        Valor = int.Parse(Console.ReadLine());
-                        Lista.RemoverFim(Valor);
-                        Console.ReadKey();
-                        break;
-                    case "5":
-                        Lista.Imprimir();
-                        Console.ReadKey();
-                        break;
+            Console.WriteLine(" ");
+            Console.WriteLine("listas atual");
+            listas.imprimir();
 
-                }
-                Console.Clear();
-            } while (Opcao != "9");
+            Console.WriteLine(" ");
+            Console.WriteLine("Deseja continuar? ");
+            continuar = Console.ReadLine();
+            continuar =continuar.ToUpper();
+            }while (continuar == "S");
+            listas.imprimir();
         }
     }
 }
-
 //LISTA DUPLAMENTE ENCADEADA ESTATICA
 ??
 
