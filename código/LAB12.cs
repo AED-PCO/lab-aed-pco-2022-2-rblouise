4f6f1c66175c6bbc473b755fd8c0d0c4ce6ee668
Louise Ribeiro <101759423+rblouise@users.noreply.github.com>
2022-12-15 16:53:09 -0300

    Update LAB12.cs
    Inserindo arvore abb

--- código/LAB12.cs
+++ código/LAB12.cs
@@ -2,7 +2,7 @@
 
 using System;
 
-namespace AVL
+namespace Arvore_AVL
 {
     class Program
     {
@@ -26,6 +26,97 @@ namespace AVL
             Console.WriteLine("5- Imprimir pre ordem.");
             Console.WriteLine("6- Imprime pos ordem.");
             Console.WriteLine("7- Sair.");
+            opcao = int.Parse(Console.ReadLine());
+
+            if(opcao == 1)
+            {
+                Console.WriteLine("Informe um valor para inserir: ");
+                valor = int.Parse(Console.ReadLine());
+
+                Arvore.inserir(valor);
+
+            }
+            
+            Console.WriteLine(" ");
+
+            if(opcao == 2){
+                bool resultado;
+
+                Console.WriteLine("Qual valor deseja pesquisar na arvore? ");
+                valor = int.Parse(Console.ReadLine());
+                resultado = Arvore.pesquisar(valor);
+
+                Console.WriteLine("O valor é {0}", resultado);
+            }
+
+             if(opcao == 3)
+            {
+               Console.WriteLine("Informe um valor para remover: ");
+               valor = int.Parse(Console.ReadLine());
+
+                Arvore.remover(valor);
+            }
+
+            if(opcao == 4)
+            {
+               Arvore.EmOrdem();
+            }
+
+            if(opcao == 5)
+            {
+              Arvore.preOrdem();
+            }
+
+            if(opcao == 6)
+            {
+                Arvore.posOrdem();
+            }
+
+            if(opcao == 7)
+            {
+                break;
+            }
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Deseja continuar? ");
+            continuar = Console.ReadLine();
+            continuar =continuar.ToUpper();
+            }while (continuar == "S");
+        }
+    }
+}
+
+//ARVORE ABB
+
+using System;
+
+namespace Arvore_ABB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArvoreBinaria Arvore = new ArvoreBinaria();
+
+            string continuar = "";
+            
+
+            do{
+            int valor = 0;
+            int opcao = 0;
+
+            Console.WriteLine("====== Arvore ABB ======");
+            Console.WriteLine(" ");
+            Console.WriteLine("Escolha uma opção: ");
+            Console.WriteLine("1- Inserir elemento.");
+            Console.WriteLine("2- Pesquisa.");
+            Console.WriteLine("3- remover.");
+            Console.WriteLine("4- Imprimir em ordem.");
+            Console.WriteLine("5- Imprimir pre ordem.");
+            Console.WriteLine("6- Imprimi pos ordem.");
+            Console.WriteLine("7- Sair");
             opcao = int.Parse(Console.ReadLine());
 
             if(opcao == 1)
